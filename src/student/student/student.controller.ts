5950441f3e9549efa33b2d6bd71019c43ca9b12c
william <williamrabs@gmail.com>
2020-07-17 11:05:43 +0200

    basic crud done

--- src/student/student/student.controller.ts
+++ src/student/student/student.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Body, Post } from '@nestjs/common';
+import { Controller, Get, Body, Post, Res, HttpStatus, Param, NotFoundException } from '@nestjs/common';
 import { StudentService } from './student.service';
+import { studentDto } from '../model/student.dto';
 
 @Controller('student')
 export class StudentController {
@@ -10,4 +11,32 @@ export class StudentController {
     console.log(studentDto);
     return this.studentService.createStudent(studentDto);
   }
+
+  // Retrieve student list
+  @Post('/create')
+    async addCustomer(@Res() res, @Body() createStudentDto: studentDto) {
+        const student = await this.studentService.createStudent(createStudentDto);
+        return res.status(HttpStatus.OK).json({
+            message: "Customer has been created successfully",
+            student
+        })
+    }
+
+    // Retrieve student list
+    @Get('/All')
+    async getAllCustomer(@Res() res) {
+        const _student = await this.studentService.getAllStudents();
+        return res.status(HttpStatus.OK).json(_student);
+    }
+
+    // Fetch a particular customer using ID
+    @Get('/:studentID')
+      async getCustomer(@Res() res, @Param('studentID') studentID) {
+          const _student = await this.studentService.getStudent(studentID);
+          if (!_student) throw new NotFoundException('Customer does not exist!');
+          return res.status(HttpStatus.OK).json(_student);
+      }
+
+
+      //implement delete by yourself
 }
--- src/student/student/student.service.ts
+++ src/student/student/student.service.ts
@@ -3,16 +3,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { student } from '../model/student.interface';
+import { timeStamp } from 'console';
 @Injectable()
 export class StudentService {
+
+     // testing
     products = [
         { id: 1, name: 'One Plus 7', price: 48000 },
         { id: 2, name: 'I Phone X', price: 64999 },
       ];
     constructor(@InjectModel('Student') private studentRepository:Model<student>){}
-    
-    
-    
+
     // testing
     getData()
     {
@@ -40,6 +41,33 @@ export class StudentService {
         return _student;
     }
 
+    /**
+     * 
+     * @param studentID id passsed from student controller through routes
+     * @param studentObject student details of update passed from student controller
+     */
 
+    async updateStudent(studentID,studentObject) : Promise<student> 
+    {
+        const _studentUpdate = await this.studentRepository.findByIdAndUpdate(studentID,studentObject,{ new: true });
+        return _studentUpdate;
+    }
+    /**
+     * 
+     * @param studentID id passsed from student controller through routes
+     * used for udating user information
+     */
+
+    async deleteStudent(studentID)
+    {
+        const deleted_student = await this.studentRepository.findByIdAndRemove(studentID);
+        return  deleted_student;
+    }
+
+     // fetch all students
+     async getAllStudents(): Promise<student[]> {
+        const _student = await this.studentRepository.find().exec();
+        return _student;
+    }
 
 }
